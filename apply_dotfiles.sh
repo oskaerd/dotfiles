b2485555f1ca4ebf61559f36e23d6a867ffdef0c
Oskar Baginski <oskbagin@gmail.com>
2023-02-06 20:53:56 +0100

    init

---
+++ apply_dotfiles.sh
@@ -0,0 +1,8 @@
+# Totaly necessary
+#sudo apt install -y cowsay fortune lolcat
+
+cp .zsh_aliases "/home/$USER"
+#cat .git_aliases >> /home/$USER/.gitconfig
+cat .zsh_append >> /home/$USER/.zshrc
+
+
